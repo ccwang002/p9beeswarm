f9f4aaba824fb7278a3f678c6a7f394c303375d4
Liang-Bo Wang <me@liang2.tw>
2026-09-15 22:55:15 -0400

    refactor: remove unused imports from __init__.py

--- src/p9beeswarm/__init__.py
+++ src/p9beeswarm/__init__.py
@@ -1,18 +1,12 @@
 """Plotnine geoms and positions modelled after R's ggbeeswarm package."""
 
-from beeswarm import beeswarm, quasirandom, sina, swarmx
-
 from .geoms import geom_beeswarm, geom_quasirandom, geom_sina
 from .positions import position_beeswarm, position_quasirandom
 
 __all__ = [
-    "beeswarm",
     "geom_beeswarm",
     "geom_quasirandom",
     "geom_sina",
     "position_beeswarm",
     "position_quasirandom",
-    "quasirandom",
-    "sina",
-    "swarmx",
 ]
